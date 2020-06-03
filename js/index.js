52f2603de0c58b22550494b65e5bc0056ac20aff
kodebae <karmenuopx@outlook.com>
2020-06-03 15:36:53 -0400

    finishjed first two tasks now attempting the whell

--- js/index.js
+++ js/index.js
@@ -1,1 +1,37 @@
 // Your code goes here
+
+//MOUSEOVER
+const purpleHeader = document.getElementById("purpleH2");
+purpleHeader.addEventListener("mouseover", function(event) {
+    // highlight the mouseover target
+    event.target.style.color = "purple"
+});
+
+// KEYDOWN
+
+addEventListener("keydown", function(event) {
+    if (event.keyCode == 86)
+        document.body.style.background = "violet";
+});
+addEventListener("keyup", function(event) {
+    if (event.keyCode == 86)
+        document.body.style.background = "";
+});
+
+//WHEEL 
+
+
+
+// DRAG / DROP
+
+//LOAD
+
+// FOCUS
+
+//RESIZE
+
+// SCROLL
+
+//SELECT
+
+//DBL
