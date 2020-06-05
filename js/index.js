9073cec172a3c0b03d55df4afddbef5dd6a121eb
kodebae <karmenuopx@outlook.com>
2020-06-04 23:59:26 -0400

    fnial chnages to code

--- js/index.js
+++ js/index.js
@@ -20,18 +20,54 @@ addEventListener("keyup", function(event) {
 
 //WHEEL 
 
-
+document.querySelector("body").addEventListener("wheel", function(e) {
+    document.querySelectorAll("p").forEach(x => {
+        e.preventDefault();
+        x.addEventListener("wheel", e => x.style.color = `rgb( ${ Math.floor( Math.random() * 256 ) }, ${ Math.floor( Math.random() * 256 ) }, ${ Math.floor( Math.random() * 256 ) } )`)
+    });
+});
 
 // DRAG / DROP
 
+document.querySelectorAll("img").forEach(image => {
+    image.addEventListener("drag", e => image.style.border = "8px dotted purple");
+});
+
 //LOAD
+
+this.addEventListener("load", e => alert("BEEP BEEP BEEP"));
 
 // FOCUS
 
+const form = document.getElementById('form');
+
+form.addEventListener('focus', (event) => {
+    event.target.style.background = 'purple';
+}, true);
+
+form.addEventListener('blur', (event) => {
+    event.target.style.background = '';
+}, true);
+
 //RESIZE
+
+this.addEventListener("resize", e => document.body.style.backgroundColor = `rgb( ${ Math.floor( Math.random() * 256 ) }, ${ Math.floor( Math.random() * 256 ) }, ${ Math.floor( Math.random() * 256 ) } )`);
 
 // SCROLL
 
+this.addEventListener("scroll", function(event) {
+    document.querySelectorAll("div").forEach(function(div) {
+        div.style.backgroundColor = "aqua ";
+    });
+});
+
 //SELECT
+
+document.querySelectorAll("h4").addEventListener("select", function(event) {
+    document.querySelectorAll("h4").forEach(function(h4) {
+        h4.style.color = "red";
+    });
+});
+
 
 //DBL
